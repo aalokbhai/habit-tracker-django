62f4085f99e669301305d6391b52d721a359f5bd
aalokbhai <aalokmiishraa@gmail.com>
2026-01-10 00:33:18 +0530

    Move SECRET_KEY and other sensitive info to environment variables

--- Habitproject/settings.py
+++ Habitproject/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d#$ixv=w_p_*o@i4btc!uq00r0m$s1z0t^*ghijy(oxy$9ky1%'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
