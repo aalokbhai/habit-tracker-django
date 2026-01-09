18b0a806185c65f08a3f7b8c1d36f72d3d24bf17
aalokbhai <aalokmiishraa@gmail.com>
2026-01-10 00:43:38 +0530

    Move SECRET_KEY and other sensitive info to environment variables

--- Habitproject/settings.py
+++ Habitproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-local-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 
